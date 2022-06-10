3a545930a958e288be5c33655f861374c5b0d294
alcasinoroyale <alexzdatny@gmail.com>
2022-06-09 23:07:56 -0400

    Create Components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
+import React from 'react';
 import './App.css';
+//Importing Components
+import Form from './components/Form';
 
 function App() {
   return (
     <div className="App">
-      <h1>To Do List</h1>
+      <header>
+        <h1>Alex's Todo List</h1>
+      </header>
+      <Form />
     </div>
   );
 }
