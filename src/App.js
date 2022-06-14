87046d13e5cf7c78f47a26ea22e0a72243e68020
alcasinoroyale <alexzdatny@gmail.com>
2022-06-14 18:23:08 -0400

    Add deletehandler

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
         <h1>Alex's Todo List {inputText}</h1>
       </header>
       <Form inputText={inputText} todos={todos} setTodos={setTodos} setInputText={setInputText}/>
-      <TodoList todos={todos} />
+      <TodoList setTodos={setTodos} todos={todos} />
     </div>
   );
 }
